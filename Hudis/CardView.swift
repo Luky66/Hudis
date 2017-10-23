8d857c6d73a389af570c1127d2b4c1edcfc77fe9
lbr <lukas.buehler@belsoft.ch>
2017-10-23 16:07:16 +0200

    Added .gitignore

---
+++ Hudis/CardView.swift
@@ -0,0 +1,21 @@
+//
+//  CardView.swift
+//  Hudis
+//
+//  Created by Lukas Bühler on 23.10.17.
+//  Copyright © 2017 Lukas Bühler. All rights reserved.
+//
+
+import UIKit
+
+class CardView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
